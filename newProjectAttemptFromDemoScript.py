adc3add0b6436ecda0c691bd8e4af81b0e55ee01
TCompuVenture <101987813+TCompuVenture@users.noreply.github.com>
2024-12-03 19:53:57 -0600

    Fix licensing

--- newProjectAttemptFromDemoScript.py
+++ newProjectAttemptFromDemoScript.py
@@ -1,13 +1,5 @@
-#
-# Copyright 2018-2023 Picovoice Inc.
-#
-# You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
-# file accompanying this source.
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-#
+# This file modified from source found here: https://github.com/Picovoice/rhino/tree/master/demo/python
+#License provided as required in this Github repo
 
 import argparse
 import struct
